1435fafa62b23aeef2ee55bd70ad2001718b6a62
ethan-xiao <308056554@qq.com>
2019-02-15 17:28:25 +0800

    Add: palindrome string

---
+++ palindrome_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPalindrome(t *testing.T) {
+	if !isPalindrome(".,") {
+		t.Fatal()
+	}
+	if !isPalindrome("a.") {
+		t.Fatal()
+	}
+	if !isPalindrome("ab a") {
+		t.Fatal()
+	}
+	if !isPalindrome("aba") {
+		t.Fatal()
+	}
+	if !isPalindrome("aa") {
+		t.Fatal()
+	}
+	if !isPalindrome("A man, a plan, a canal: Panama") {
+		t.Fatal()
+	}
+	if !isPalindrome("Aman, a plan, a canal: Panama") {
+		t.Fatal()
+	}
+
+	if isPalindrome("ab") {
+		t.Fatal()
+	}
+	if isPalindrome("A man, a plan, a canal: Panaa") {
+		t.Fatal()
+	}
+	if isPalindrome("Aman, a plan, a canal: Panaa") {
+		t.Fatal()
+	}
+}
+
+func isPalindrome(s string) bool {
+	i := 0
+	j := len(s) - 1
+	var a, b byte
+	for i < j {
+		for a = 0; i <= j; i++ {
+			if a = toAlphanumeric(s[i]); a != 0 {
+				break
+			}
+		}
+		for b = 0; i <= j; j-- {
+			if b = toAlphanumeric(s[j]); b != 0 {
+				break
+			}
+		}
+		if a != b {
+			return false
+		}
+		i++
+		j--
+	}
+	// i >= j
+	// a == b or (a != b and (a == 0 or b == 0))
+	return a == b
+}
+
+func toAlphanumeric(c byte) byte {
+	if (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') {
+		return c
+	}
+	if c >= 'A' && c <= 'Z' {
+		return c + 32
+	}
+	return 0
+}
